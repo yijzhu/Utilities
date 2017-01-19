8d6d8ccddf18a5cd2de6a3287367c8f11f99cf5e
yijzhu <thierry_1412@sina.com>
2017-01-20 00:12:45 +0800

    Handle circle

--- deepCopy.js
+++ deepCopy.js
@@ -22,43 +22,74 @@ function deepCopyByRecursion(obj) {
 }
 
 function deepCopyByBFS(obj) {
+	function getParentAndCurrentKey(path, root) {
+		if (!path) {
+			return {
+				curKey : path,
+				parent : root
+			};
+		}
+		var keys = path.split('.');
+		var curKey = keys[keys.length - 1];
+		var parent = keys.slice(0, -1).reduce(function(obj, key) {
+			return obj[key];
+		}, root);
+		return {
+			curKey : curKey,
+			parent : parent
+		}
+	}
+
 	if (obj) {
-		var queue = [];
+		var queue = [], visited = [];
 		// Push the root element
 		queue.push({
-			key : '',
+			path : '',
 			value : obj	
 		});
 
 		var newObj;
 		while (queue.length > 0) {
 			var item = queue.shift();
-			var parent, curKey;
+			var typeStr = Object.prototype.toString.apply(item.value);
+
+			// Handle circle
+			if (typeStr === '[object Object]' || typeStr === '[object Array]') {
+				var circleTargetItem = visited.filter(function(visitedItem) {
+					return item.value === visitedItem.value;
+				})[0];
+				if (circleTargetItem) {
+					var circleRet = getParentAndCurrentKey(circleTargetItem.key, newObj);
+					var circleParent = circleRet.parent, circleCurKey = circleRet.curKey;
+					var circleTarget = circleCurKey ? circleParent[circleCurKey] : circleParent;
+
+					var ret = getParentAndCurrentKey(item.path, newObj);
+					var parent = ret.parent, curKey = ret.curKey;
+					// parent must exist, otherwise there is no circle at all
+					parent[curKey] = circleTarget;
+					continue;
+				}
+			} 
+
+			// Not push circle element
+			visited.push(item);
 
 			// Get parent obj
-			if (item.key) {
-				var keys = item.key.split('.');
-				curKey = keys[keys.length - 1];
-				parent = keys.slice(0, -1).reduce(function(obj, key) {
-					return obj[key];
-				}, newObj);
-			}
+			var ret = getParentAndCurrentKey(item.path, newObj);
+			var parent = ret.parent, curKey = ret.curKey;
 
-			var typeStr = Object.prototype.toString.apply(item.value);
 			var val;
 			if (typeStr === '[object Object]' || typeStr === '[object Array]') {
 				val = typeStr === '[object Object]' ? {} : [];
 				// Push children
 				Object.keys(item.value).forEach(function(key) {
 					queue.push({
-						key : item.key ? item.key + '.' + key : key,
+						path : item.key ? item.key + '.' + key : key,
 						value : item.value[key]
 					})				
 				});
 			} else if (typeStr === '[object Function]') {
-				val = function() {
-					return item.value.apply(this, arguments);
-				}
+				val = function() { return item.value.apply(this, arguments); }
 			} else {
 				val = item.value;
 			}
@@ -66,6 +97,7 @@ function deepCopyByBFS(obj) {
 			if (parent && curKey) {
 				parent[curKey] = val;
 			} else {
+				// Init newObj, if and only if the item is the first element
 				newObj = val;
 			}	
 		}

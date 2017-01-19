4321ad2d88c15e88611ab83955896ef45151643e
yijzhu <thierry_1412@sina.com>
2017-01-19 22:26:38 +0800

    Add deep copy by BFS method

--- deepCopy.js
+++ deepCopy.js
@@ -1,4 +1,4 @@
-function deepCopy(obj) {
+function deepCopyByRecursion(obj) {
 	if (obj) {
 		var typeStr = Object.prototype.toString.apply(obj)
 		if (typeStr === '[object Object]') {
@@ -18,5 +18,58 @@ function deepCopy(obj) {
 		}
 	} 
 
+	return obj;
+}
+
+function deepCopyByBFS(obj) {
+	if (obj) {
+		var queue = [];
+		// Push the root element
+		queue.push({
+			key : '',
+			value : obj	
+		});
+
+		var newObj;
+		while (queue.length > 0) {
+			var item = queue.shift();
+			var parent, curKey;
+
+			// Get parent obj
+			if (item.key) {
+				var keys = item.key.split('.');
+				curKey = keys[keys.length - 1];
+				parent = keys.slice(0, -1).reduce(function(obj, key) {
+					return obj[key];
+				}, newObj);
+			}
+
+			var typeStr = Object.prototype.toString.apply(item.value);
+			var val;
+			if (typeStr === '[object Object]' || typeStr === '[object Array]') {
+				val = typeStr === '[object Object]' ? {} : [];
+				// Push children
+				Object.keys(item.value).forEach(function(key) {
+					queue.push({
+						key : item.key ? item.key + '.' + key : key,
+						value : item.value[key]
+					})				
+				});
+			} else if (typeStr === '[object Function]') {
+				val = function() {
+					return obj.apply(this, arguments);
+				}
+			} else {
+				val = item.value;
+			}
+
+			if (parent && curKey) {
+				parent[curKey] = val;
+			} else {
+				newObj = val;
+			}	
+		}
+		return newObj;
+	}
 	return obj;
 }
